8494855946db66a35751d82d7d1c15478d3c7d39
dilara153232@gmail.com <dilara153232@gmail.com>
2021-04-14 00:04:48 +0300

    Add project files.

---
+++ Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [StringLength(200)]
+        public string CategoryDescription { get; set; }
+        public bool Status { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+    }
+
+}
---
+++ Entities/Writer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class Writer
+    {
+
+        [Key]
+        public int WriterId { get; set; }
+
+        [StringLength(50)]
+        public string WriterName { get; set; }
+
+        [StringLength(50)]
+        public string WriterSurName { get; set; }
+
+        [StringLength(150)]
+        public string WriterImage { get; set; }
+
+        [StringLength(50)]
+        public string WriterMail { get; set; }
+
+        [StringLength(20)]
+        public string WriterPassword { get; set; }
+
+        public int HeadingId { get; set; }
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
+    }
+}
