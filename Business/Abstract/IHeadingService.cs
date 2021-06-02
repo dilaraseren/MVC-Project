0eb154838a2fc6b2956f2eb8f544e7e7b70e4550
dilara153232@gmail.com <dilara153232@gmail.com>
2021-06-02 17:01:56 +0300

    02.06.2021

---
+++ Business/Abstract/IHeadingService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetList();
+        void HeadingAdd(Heading heading);
+        Heading GetById(int id);
+        void HeadingDelete(Heading heading);
+
+        void HeadingUpdate(Heading heading);
+    }
+}
---
+++ Business/Concrete/HeadingManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class HeadingManager : IHeadingService
+    {
+        IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+
+        public Heading GetById(int id)
+        {
+            return _headingDal.Get(x => x.HeadingId == id);
+        }
+
+        public List<Heading> GetList()
+        {
+            return _headingDal.List();
+        }
+
+        public void HeadingAdd(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            _headingDal.Delete(heading);
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
+    }
+}
